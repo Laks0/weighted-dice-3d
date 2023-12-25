bf5e696f7ba7d1a54dc938906a63f2f7ec41d49b
laks0 <laksjoaquin@gmail.com>
2023-12-25 17:54:44 -0300

    Bet Hud: Game Time

--- Classes/HUD/HUD.gd
+++ Classes/HUD/HUD.gd
@@ -2,6 +2,7 @@ extends Control
 class_name ArenaHUD
 
 @export var arena : Arena
+var time = 0.0
 
 func _ready():
 	arena.connect("effectStarted", effectStarted)
@@ -9,6 +10,8 @@ func _ready():
 	if BetHandler.currentBet is MostGragsBet:
 		$MostGrabsDisplay.start(arena)
 		$MostGrabsDisplay.visible = true
+	elif BetHandler.currentBet is GameTimeBet:
+		$GameTime.visible = true
 
 func effectStarted(effect : Effect):
 	$EffectName.text = effect.effectName
@@ -17,3 +20,7 @@ func effectStarted(effect : Effect):
 	tween.tween_property($EffectName, "modulate", Color.WHITE, .5)
 	tween.tween_interval(2)
 	tween.tween_property($EffectName, "modulate", Color.TRANSPARENT, .5)
+
+func _process(delta):
+	time += delta
+	$GameTime.text = "%04.1f" % time
