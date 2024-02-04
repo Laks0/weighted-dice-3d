3293838acf52f75c46701703bf9cdcdd4a9899fd
laks0 <laksjoaquin@gmail.com>
2024-02-04 19:40:31 -0300

    Fix problema con iniciar los ataques del dado en ready

--- Classes/Monigotes/DeathParticles.gd
+++ Classes/Monigotes/DeathParticles.gd
@@ -3,5 +3,7 @@ extends GPUParticles3D
 @export var mesh : PrismMesh
 
 func _ready():
+	# Para que cada una tenga un color distinto hay que duplicar
+	# el mesh en cada monigote
 	draw_pass_1 = mesh.duplicate(true)
 	draw_pass_1.material.albedo_color = get_parent().player.color
