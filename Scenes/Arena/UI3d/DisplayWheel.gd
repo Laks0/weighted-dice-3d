4b5980ca252f52a6025ff832d4c23f00a1986c07
Laks0 <laksjoaquin@gmail.com>
2024-02-16 16:01:49 -0300

    Objeto básico de contador

---
+++ Scenes/Arena/UI3d/DisplayWheel.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+func _rotateWheelTo(wheelN : int, n : int):
+	var wheel : Node3D = get_node("Wheel_" + str(wheelN))
+	var currentN := _getWheelCurrentFace(wheel)
+
+	var rotationTween := create_tween()
+	var deltaRotation = PI/5 * posmod((n - currentN), 10)
+	rotationTween.tween_property(wheel, "rotation:x", wheel.rotation.x + deltaRotation, .1)
+
+## Las ruedas empiezan en PI/10 y giran PI/5 por cada número
+func _getWheelCurrentFace(wheel : Node3D) -> int:
+	return (wheel.rotation.x - PI/10) / (PI/5)
+
+func _process(delta):
