8b390c688ae4ca47246abd0fba485a42f0d5cfe1
Laks0 <laksjoaquin@gmail.com>
2024-03-02 16:14:22 -0300

    Mejores señalizadores MostUsedArea y sombras 2d

--- Assets/Bets/AreaUse.gd
+++ Assets/Bets/AreaUse.gd
@@ -4,7 +4,7 @@ var playersInArea := [.0,.0,.0,.0]
 
 func _process(_delta):
 	var totalPlayers : float = PlayerHandler.getPlayersAlive().size()
-	$SE.transparency = 1 - playersInArea[MostUsedAreaBet.ArenaSide.BOTTOM_RIGHT] / (totalPlayers+3)
-	$SW.transparency = 1 - playersInArea[MostUsedAreaBet.ArenaSide.BOTTOM_LEFT] / (totalPlayers+3)
-	$NE.transparency = 1 - playersInArea[MostUsedAreaBet.ArenaSide.TOP_RIGHT] / (totalPlayers+3)
-	$NW.transparency = 1 - playersInArea[MostUsedAreaBet.ArenaSide.TOP_LEFT] / (totalPlayers+3)
+	$SE.transparency = 1 - playersInArea[MostUsedAreaBet.ArenaSide.BOTTOM_RIGHT] / (totalPlayers+4)
+	$SW.transparency = 1 - playersInArea[MostUsedAreaBet.ArenaSide.BOTTOM_LEFT] / (totalPlayers+4)
+	$NE.transparency = 1 - playersInArea[MostUsedAreaBet.ArenaSide.TOP_RIGHT] / (totalPlayers+4)
+	$NW.transparency = 1 - playersInArea[MostUsedAreaBet.ArenaSide.TOP_LEFT] / (totalPlayers+4)
