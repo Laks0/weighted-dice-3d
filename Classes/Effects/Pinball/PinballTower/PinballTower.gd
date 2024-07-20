1d6db54368dd2406787731ab4bffd7fb258f00f2
Laks0 <laksjoaquin@gmail.com>
2024-07-20 20:49:00 -0300

    No te podés trabar más en las torres de pinball

--- Classes/Effects/Pinball/PinballTower/PinballTower.gd
+++ Classes/Effects/Pinball/PinballTower/PinballTower.gd
@@ -7,3 +7,13 @@ func _on_area_3d_body_entered(body):
 		return
 	
 	body.knockbackFrom(position, throwForce)
+	
+	# Aleja el monigote por si se queda atrapado adentro de la torre
+	var distance := position.distance_to(body.position)
+	var radius : float = $Area3D/CollisionShape3D.shape.radius
+	
+	if distance > radius:
+		return
+	
+	var dir := position.direction_to(body.position)
+	body.position = position + dir * radius
