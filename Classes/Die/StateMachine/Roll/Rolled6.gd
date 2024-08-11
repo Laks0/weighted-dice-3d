fa1177c67c678fa6f7932ac265c4f028b08016b1
Laks0 <laksjoaquin@gmail.com>
2024-08-11 15:38:37 -0300

    Mejor animación de salida del dado en 6

--- Classes/Die/StateMachine/Roll/Rolled6.gd
+++ Classes/Die/StateMachine/Roll/Rolled6.gd
@@ -3,8 +3,11 @@ extends State
 
 func _on_enter(_args):
 	target.freeze = true
-	var outTween := get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
-	outTween.tween_property(target, "position", Vector3(-9, 5, 0), 1)
+	var outTween := get_tree().create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	outTween.tween_property(target, "scale", Vector3.ZERO, .4)
+	outTween.tween_callback(func(): 
+		target.position = Vector3(-10, 3, 0)
+		target.scale = Vector3.ONE)
 	
 	var effect : Effect = target.get_parent().effects[5]
 	get_tree().create_timer(effect.duration).timeout.connect(change_to_next)
