96ec10731c11e1a36bba1b0d9eea963d9e385f1a
CDS-Seb <60557242+CDS-Seb@users.noreply.github.com>
2024-08-14 00:17:26 -0300

    hotkey para el mute

--- Autoload/DebugVars.gd
+++ Autoload/DebugVars.gd
@@ -12,3 +12,9 @@ var dontStartGame := false
 var dropAllChips := false
 
 var inmortalMonigotes := false
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_mute_ost"):
+		AudioServer.set_bus_mute(1, !AudioServer.is_bus_mute(1))
+	if Input.is_action_just_pressed("ui_mute_sfx"):
+		AudioServer.set_bus_mute(2, !AudioServer.is_bus_mute(2))
