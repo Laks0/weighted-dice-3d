8d74f6f25e63fc92363813e4f3220e562d8666ed
Laks0 <laksjoaquin@gmail.com>
2024-09-04 22:27:23 -0300

    Nuevo pinball

--- Classes/Effects/Pinball/PinballTower/PinballTower.gd
+++ Classes/Effects/Pinball/PinballTower/PinballTower.gd
@@ -1,12 +1,16 @@
 extends StaticBody3D
 
-@export var throwForce : float = 20
+@export var throwForce : float = 30
 
 func _on_area_3d_body_entered(body):
 	if not body is Monigote:
 		return
 	
 	body.knockbackFrom(position, throwForce)
+	
+	var original_scale : Vector3 = $Mesh.scale
+	create_tween().tween_property($Mesh, "scale", original_scale * 1.5, .1)
+	create_tween().tween_property($Mesh, "scale", original_scale, .1)
 	
 	# Aleja el monigote por si se queda atrapado adentro de la torre
 	var distance := position.distance_to(body.position)
