f93ee2ae5a753d1f332d102e02f110bcbf574f28
Laks0 <laksjoaquin@gmail.com>
2025-04-04 00:16:09 -0300

    Fix errores en el rework de la UI Funciones que habían quedado en el botón deberían estar en la clase padre + error con visibilidades en vecinos

--- Classes/CustomUI/GamepadSelectButton/GamepadSelectButton.gd
+++ Classes/CustomUI/GamepadSelectButton/GamepadSelectButton.gd
@@ -6,11 +6,6 @@ signal pressed
 
 @export var text : String
 
-## Automáticamente cada frame se fija si está en un hbox o un vbox y setea sus
-## vecinos dependiendo de los nodos en su nivel del árbol
-@export var autoSetNeighbours := true
-@export_tool_button("Set automatic neighbours") var automaticButton = setAutomaticNeighbours
-@export_tool_button("Reset neighbours") var resetButton = resetNeighbours
 
 @export_group("Texturas")
 @export var normalTexture  : Texture
@@ -31,9 +26,6 @@ func _game_and_editor_process(_delta):
 	if underTexture != null:
 		%UnderTexture.texture = underTexture
 	
-	if autoSetNeighbours:
-		setAutomaticNeighbours()
-	
 	if !focused or !is_visible_in_tree():
 		return
 	
@@ -49,54 +41,6 @@ func _in_game_process(_delta):
 	if Input.is_action_just_pressed(actions["grab"]):
 		pressed.emit()
 		%OnclickedTime.start()
-
-func resetNeighbours():
-	var directionsPrefix := ["right", "down", "up", "left"]
-	for prefix in directionsPrefix:
-		set(prefix + "Select", null)
-		set(prefix + "Fallback", null)
-
-## Establece los vecinos del botón automáticamente teniendo en cuenta el orden de
-## los hijos del container al que pertenece (solo funciona cuando el nodo está en
-## un hbox o vbox)
-func setAutomaticNeighbours():
-	var increasingNeighbourDirectionPrefix : String
-	var decreasingNeighbourDirectionPrefix : String
-	if get_parent() is HBoxContainer:
-		increasingNeighbourDirectionPrefix = "right"
-		decreasingNeighbourDirectionPrefix = "left"
-	elif get_parent() is VBoxContainer:
-		increasingNeighbourDirectionPrefix = "down"
-		decreasingNeighbourDirectionPrefix = "up"
-	else:
-		return
-	
-	var index := get_index()
-	var nextIndex := -1
-	var prevIndex := 10000
-	var firstIndex := -1
-	var lastIndex := -1
-	var container := get_parent()
-	for i in range(container.get_child_count()):
-		var child = container.get_child(i)
-		if not child is GamepadSelectButton:
-			continue
-		lastIndex = i
-		if firstIndex == -1:
-			firstIndex = i
-		if i < index:
-			prevIndex = i
-		if i > index and nextIndex == -1:
-			nextIndex = i
-	
-	# Para el wrap arround
-	nextIndex = max(nextIndex, firstIndex)
-	prevIndex = min(prevIndex, lastIndex)
-	
-	set(increasingNeighbourDirectionPrefix + "Select", container.get_child(nextIndex))
-	set(increasingNeighbourDirectionPrefix + "Fallback", container.get_child(nextIndex))
-	set(decreasingNeighbourDirectionPrefix + "Select", container.get_child(prevIndex))
-	set(decreasingNeighbourDirectionPrefix + "Fallback", container.get_child(prevIndex))
 
 func setLabelColor(color : Color):
 	%Label.label_settings.font_color = color
--- Classes/CustomUI/GamepadSelectUI.gd
+++ Classes/CustomUI/GamepadSelectUI.gd
@@ -15,7 +15,15 @@ signal movedRight
 @export var focused  := false
 @export var disabled := false
 
-@export_group("Vecinos")
+@export_group("Neighbours")
+
+## Automáticamente cada frame se fija si está en un hbox o un vbox y setea sus
+## vecinos dependiendo de los nodos en su nivel del árbol
+@export var autoSetNeighbours := true
+
+@export_tool_button("Set automatic neighbours") var automaticButton = setAutomaticNeighbours
+@export_tool_button("Reset neighbours") var resetButton = resetNeighbours
+
 @export var upSelect    : GamepadSelectUI
 @export var downSelect  : GamepadSelectUI
 @export var leftSelect  : GamepadSelectUI
@@ -25,6 +33,7 @@ signal movedRight
 @export var downFallback  : GamepadSelectUI
 @export var leftFallback  : GamepadSelectUI
 @export var rightFallback : GamepadSelectUI
+
 
 enum {UP, DOWN, LEFT, RIGHT, NONE}
 
@@ -36,6 +45,10 @@ func _ready():
 
 func _process(delta):
 	_game_and_editor_process(delta)
+	
+	if autoSetNeighbours:
+		setAutomaticNeighbours()
+	
 	if Engine.is_editor_hint():
 		return
 	actions = Controllers.getActions(controller)
@@ -86,11 +99,12 @@ func changeFocus(newFocus : GamepadSelectUI, dir : int):
 	newFocus.focus(dir)
 
 func unfocus():
+	if focused:
+		endedFocus.emit()
 	focused = false
-	endedFocus.emit()
 
 func focus(dir : int = LEFT):
-	if disabled:
+	if disabled or not visible:
 		if dir == LEFT:
 			changeFocus(leftFallback, LEFT)
 		elif dir == RIGHT:
@@ -117,3 +131,54 @@ func disable():
 
 func enable():
 	disabled = false
+
+func resetNeighbours():
+	var directionsPrefix := ["right", "down", "up", "left"]
+	for prefix in directionsPrefix:
+		set(prefix + "Select", null)
+		set(prefix + "Fallback", null)
+
+## Establece los vecinos del botón automáticamente teniendo en cuenta el orden de
+## los hijos del container al que pertenece (solo funciona cuando el nodo está en
+## un hbox o vbox)
+func setAutomaticNeighbours():
+	var increasingNeighbourDirectionPrefix : String
+	var decreasingNeighbourDirectionPrefix : String
+	if get_parent() is HBoxContainer:
+		increasingNeighbourDirectionPrefix = "right"
+		decreasingNeighbourDirectionPrefix = "left"
+	elif get_parent() is VBoxContainer:
+		increasingNeighbourDirectionPrefix = "down"
+		decreasingNeighbourDirectionPrefix = "up"
+	else:
+		return
+	
+	var index := get_index()
+	var nextIndex := -1
+	var prevIndex := 10000
+	var firstIndex := -1
+	var lastIndex := -1
+	var container := get_parent()
+	for i in range(container.get_child_count()):
+		var child = container.get_child(i)
+		if not child is GamepadSelectButton:
+			continue
+		if not child.visible:
+			continue
+		
+		lastIndex = i
+		if firstIndex == -1:
+			firstIndex = i
+		if i < index:
+			prevIndex = i
+		if i > index and nextIndex == -1:
+			nextIndex = i
+	
+	# Para el wrap arround
+	nextIndex = max(nextIndex, firstIndex)
+	prevIndex = min(prevIndex, lastIndex)
+	
+	set(increasingNeighbourDirectionPrefix + "Select", container.get_child(nextIndex))
+	set(increasingNeighbourDirectionPrefix + "Fallback", container.get_child(nextIndex))
+	set(decreasingNeighbourDirectionPrefix + "Select", container.get_child(prevIndex))
+	set(decreasingNeighbourDirectionPrefix + "Fallback", container.get_child(prevIndex))
