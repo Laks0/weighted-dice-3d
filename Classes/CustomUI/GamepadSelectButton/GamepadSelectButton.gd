a2825949c4c5d2b38ebb07ed7d69d9d0f34b01d1
Laks0 <laksjoaquin@gmail.com>
2025-07-12 20:38:21 -0300

    Refactor estructura de los botones

--- Classes/CustomUI/GamepadSelectButton/GamepadSelectButton.gd
+++ Classes/CustomUI/GamepadSelectButton/GamepadSelectButton.gd
@@ -6,33 +6,42 @@ signal pressed
 
 @export var text : String
 
+@export_group("Textures")
+@export var normalTexture  : StyleBox
+@export var focusedTexture : StyleBox
+@export var pressedTexture : StyleBox
 
-@export_group("Texturas")
-@export var normalTexture  : Texture
-@export var focusedTexture : Texture
-@export var pressedTexture : Texture
-@export var underTexture : Texture
+@export_group("Icon")
+@export var movableIconNormalTexture  : Texture
+@export var movableIconFocusedTexture : Texture
+@export var movableIconPressedTexture : Texture
+@export var movableIconUnderTexture : Texture
+@export var movableIconPosition := Vector2.ZERO
+@export var movableIconScale := Vector2.ONE
 
 func _game_and_editor_process(_delta):
 	%Label.text = text
 	if !focused:
-		%OverTexture.texture = normalTexture
+		_updateTexture(normalTexture, movableIconNormalTexture)
 	
 	if disabled:
 		modulate.a = .5
 	else:
 		modulate.a = 1
 	
-	if underTexture != null:
-		%UnderTexture.texture = underTexture
+	if movableIconUnderTexture != null:
+		%MovableIconUnderTexture.texture = movableIconUnderTexture
+	
+	%MovableIcon.position = movableIconPosition
+	%MovableIcon.scale = movableIconScale
 	
 	if !focused or !is_visible_in_tree():
 		return
 	
 	if %OnclickedTime.is_stopped():
-		%OverTexture.texture = focusedTexture
+		_updateTexture(focusedTexture, movableIconFocusedTexture)
 	else:
-		%OverTexture.texture = pressedTexture
+		_updateTexture(pressedTexture, movableIconPressedTexture)
 
 func _in_game_process(_delta):
 	if !focused or !is_visible_in_tree():
@@ -44,3 +53,7 @@ func _in_game_process(_delta):
 
 func setLabelColor(color : Color):
 	%Label.label_settings.font_color = color
+
+func _updateTexture(backgroundTexture : StyleBox, iconTexture : Texture):
+	add_theme_stylebox_override("panel", backgroundTexture)
+	%MovableIcon.texture = iconTexture
