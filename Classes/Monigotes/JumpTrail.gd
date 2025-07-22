514f334f0be91b5409ac88c5662b73484610dc02
Laks0 <laksjoaquin@gmail.com>
2025-07-22 17:27:39 -0300

    Sin flicker en el trail

--- Classes/Monigotes/JumpTrail.gd
+++ Classes/Monigotes/JumpTrail.gd
@@ -12,15 +12,16 @@ func _ready():
 
 func _process(delta):
 	super(delta)
-	var vel2d := Vector2(mon.velocity.x, mon.velocity.z).normalized()
+
+func onJumpStarted():
+	var vel2d := mon.unclampedVelocity.normalized()
 	var angleToVertical = acos(vel2d.dot(Vector2.UP))
-	if (angleToVertical >= 0 and angleToVertical <= PI/4) or \
-		(angleToVertical >= .75*PI and angleToVertical <= PI): 
+	if (angleToVertical >= 0 and angleToVertical < PI/4) or \
+		(angleToVertical > .75*PI and angleToVertical <= PI): 
 		rotation.z = PI/2
 	else:
 		rotation.z = 0
-
-func onJumpStarted():
+	
 	restart()
 	visible = true
 
