1613bc97454078f1873b2a1d19d0810fb0515626
Laks0 <laksjoaquin@gmail.com>
2025-07-22 16:57:59 -0300

    angulo correcto para el trail de saltos

--- Classes/Monigotes/JumpTrail.gd
+++ Classes/Monigotes/JumpTrail.gd
@@ -10,6 +10,16 @@ func _ready():
 	texture.gradient.colors[0] = playerColor.lightened(.1)
 	texture.gradient.colors[1] = playerColor.darkened(.9)
 
+func _process(delta):
+	super(delta)
+	var vel2d := Vector2(mon.velocity.x, mon.velocity.z).normalized()
+	var angleToVertical = acos(vel2d.dot(Vector2.UP))
+	if (angleToVertical >= 0 and angleToVertical <= PI/4) or \
+		(angleToVertical >= .75*PI and angleToVertical <= PI): 
+		rotation.z = PI/2
+	else:
+		rotation.z = 0
+
 func onJumpStarted():
 	restart()
 	visible = true
