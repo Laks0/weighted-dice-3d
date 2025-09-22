1899760572eeaecf5cfcac040544b74eeb8c1157
Laks0 <laksjoaquin@gmail.com>
2025-09-22 16:39:18 -0300

    Optimización de público

--- Common/Hover.gd
+++ Common/Hover.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var frequency : float = 1
 
 func _ready():
+	await get_tree().create_timer(randf_range(.01,.03)).timeout
 	var tween := create_tween().set_loops()\
 		.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
 	tween.tween_property(self, "position:y", position.y + yDisplacement, frequency/2)
--- Scenes/Arena/Audience/AudienceMember.gd
+++ Scenes/Arena/Audience/AudienceMember.gd
@@ -1,16 +1,18 @@
 extends Sprite3D
 
-@onready var idleXPosition = texture.region.position.x
-@onready var jumpingXPosition = texture.region.position.x + texture.region.size.x * 4
+@export var idleTextures : Array[Texture2D]
+@export var jumpingTextures : Array[Texture2D]
+
+var id : int = 0
 
 func _startIdle() -> void:
-	texture.region.position.x = idleXPosition
+	texture = idleTextures[id]
 	$IdleTimer.start(randf_range(.9, 3))
 
 func _startJump() -> void:
-	texture.region.position.x = jumpingXPosition
+	texture = jumpingTextures[id]
 	$JumpingTimer.start(randf_range(.3, 1))
 
 func _ready() -> void:
-	texture.region.position.y += texture.region.size.y * randi_range(0, 9)
+	id = randi_range(0, 7)
 	_startIdle()
